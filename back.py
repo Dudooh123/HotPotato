8b57cbf48f7a1cb8e4738a42ef38e059425d4978
nukennin07 <brunopossanic@gmail.com>
2023-10-29 15:01:33 -0300

    interface

--- back.py
+++ back.py
@@ -8,9 +8,8 @@
 #3. onde a batata quente parar a pessoa tera de sair do jogo
 #4. ganha quem ficar por ultimo
 
-import random
-
-seq_contagem = random.randint(1, 10)
+def tirarquadrado():
+    return None
 
 def lista_vazia(array):
     if not array:
@@ -27,6 +26,7 @@ def enQueue(array, valor): #Adiciona elementos na fila sendo que o primeiro elem
 def dequeue(array):
     if not array:  # Verifica se a lista esta vazia
         return None  # retorna nulo caso esteja
+    tirarquadrado()
     return array.pop(0)
 
 
@@ -34,7 +34,7 @@ def fila_circular(array):
     if len(array) < 2:
         print("Elementos insuficientes.")
     else:
-        for event in range(seq_contagem): #Roda array 5 vezes sendo que os elemmentoss do array se movem para a equerda
+        for event in range(tempo): #Roda array 5 vezes sendo que os elemmentoss do array se movem para a equerda
             primeiro_elemento = array[0]
             array[0:-1] = array[1:]  # Movendo todos os elementos uma posição para a esquerda
             array[-1] = primeiro_elemento  # Colocando o primeiro elemento na última posição
@@ -45,7 +45,7 @@ def fila_circular(array):
 on = True
 participantes = []
 count = 1
-
+tempo = int(input("Informe quantos rounds: "))
 while on:
     nome_participante = input(f'\n Informe o nome do participante {count} \n Ou digite (1) para inicar o jogo: ').lower()
     participantes = enQueue(participantes, nome_participante) #Adicionando dados a fila
@@ -58,5 +58,5 @@ while on:
         while len(participantes) > 1:
             fila_circular(participantes)
             print(participantes)
-            print(f"A sequencia de contagem foi {seq_contagem}.")
+            print(f"A sequencia de contagem foi {tempo}.")
         on = False
